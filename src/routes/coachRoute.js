6a1895ed7c208186945f43b1ec9997b3d50a154c
GitSaka <sakaayedidjou@gmail.com>
2026-05-01 13:17:19 +0200

    feat: dashboard coach dynamique avec agrégations MongoDB et graphiques réels

--- src/routes/coachRoute.js
+++ src/routes/coachRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getCoachEarnings,getCoachDashboardData} =  require('../controllers/coachController')
+const auth = require('../middlewares/authMiddleware');
 
 router.get('/earnings',getCoachEarnings);
-router.get('/stats',getCoachDashboardData);
+router.get('/stats',auth,getCoachDashboardData);
 
 
 module.exports = router
